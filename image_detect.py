5c7bd7a19ee94ade7466b5289ef688c67218438b
sheng chen <Chen_Sheng19@163.com>
2020-05-07 18:54:25 +0800

    execute object detection

---
+++ image_detect.py
@@ -0,0 +1,65 @@
+#! usr/bin/env python
+# coding:utf-8
+#=====================================================
+# Copyright (C) 2020 * Ltd. All rights reserved.
+#
+# Author      : Chen_Sheng19
+# Editor      : VIM
+# Create time : 2020-05-07
+# File name   : image_detect.py 
+# Description : execute object detection on one image
+#
+#=====================================================
+
+import cv2
+import numpy as np
+import tensorflow as tf
+from yolo3 import yolo3,decode
+import utils
+from PIL import Image
+
+input_size = 416
+image_path = "./predict_image/hourse.jpg"
+
+input_layer = tf.keras.layers.Input([input_size,input_size,3])
+feature_maps = yolo3(input_layer)
+
+original_image = cv2.imread(image_path)
+original_image = cv2.cvtColor(original_image,cv2.COLOR_BGR2RGB)
+original_image_size = original_image.shape[:2]
+
+image_data = utils.image_preprocess(np.copy(original_image),(input_size,input_size))
+image_data = image_data[np.newaxis,...].astype(np.float32)
+
+bbox_tensors = []
+for i,fm in enumerate(feature_maps):
+    bbox_tensor = decode(fm,i)
+    bbox_tensors.append(bbox_tensor)
+
+model = tf.keras.Model(input_layer,bbox_tensors)
+utils.load_weights(model,"./yolov3.weights")
+model.summary()
+
+pred_bbox = model.predict(image_data)
+pred_bbox = [tf.reshape(x,tf.shape(x)[-1]) for x in pred_bbox]
+pred_bbox = tf.concat(pred_bbox,axis=0)
+bboxes = utils.postprocess_boxes(pred_bbox,original_image_size,input_size,0.3)
+bboxes = utils.nms(bboxes,0.45)
+
+image = utils.draw_bbox(original_image,bboxes)
+image = Image.fromarray(image)
+image.show()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- utils.py
+++ utils.py
@@ -211,3 +211,92 @@ def load_weights(model,weights_file):
     assert len(wf.read()) == 0
     wf.close()
 
+def postprocess_boxes(pred_bbox,org_img_shape,input_size,score_threshold):
+    """
+    Argument:
+        pred_bbox       : dim = (num_box,5+num_class)
+        org_img_shape   : the h and w of original image,(h,w)
+        input_size      : the h and w of input layer,(h,w)
+        score_threshold :
+    Return:
+        
+    """
+    valid_scale = [0,np.inf]
+    pred_bbox = np.array(pred_bbox)
+
+    pred_xywh = pred_bbox[:,0:4]
+    pred_conf = pred_bbox[:,4]
+    pred_prob = pred_bbox[:,5:]
+
+    pred_coor = np.concatenate([pred_xywh[:,:2] - pred_xywh[:,2:4] * 0.5,
+                                pred_xywh[:,:2] + pred_xywh[:,2:4] * 0.5],axis = -1)
+
+    org_h,org_w = org_img_shape
+    resize_ratio = min(input_size / org_h,input_size / org_w)
+
+    dw = (input_size - resize_ratio * org_w) / 2
+    dh = (input_size - resize_ratio * org_h) / 2
+
+    pred_coor[:,0::2] = 1.0 * (pred_coor[:,0::2] - dw) / resize_ratio
+    pred_coor[:,1::2] = 1.0 * (pred_coor[:,1::2] - dh) / resize_ratio
+
+    pred_coor = np.concatenate([np.maximum(pred_coor[:, :2], [0, 0]),
+                                np.minimum(pred_coor[:, 2:], [org_w - 1, org_h - 1])], axis=-1)
+    invalid_mask = np.logical_or((pred_coor[:, 0] > pred_coor[:, 2]), (pred_coor[:, 1] > pred_coor[:, 3]))
+    pred_coor[invalid_mask] = 0
+
+    bboxes_scale = np.sqrt(np.multiply.reduce(pred_coor[:, 2:4] - pred_coor[:, 0:2], axis=-1))
+    scale_mask = np.logical_and((valid_scale[0] < bboxes_scale), (bboxes_scale < valid_scale[1]))
+
+    classes = np.argmax(pred_prob, axis=-1)
+    scores = pred_conf * pred_prob[np.arange(len(pred_coor)), classes]
+    score_mask = scores > score_threshold
+    mask = np.logical_and(scale_mask, score_mask)
+    coors, scores, classes = pred_coor[mask], scores[mask], classes[mask]
+
+    return np.concatenate([coors, scores[:, np.newaxis], classes[:, np.newaxis]], axis=-1)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
