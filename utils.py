1f5839ca8235434c34709435a06bee4cdbf4a8e3
sheng chen <Chen_Sheng19@163.com>
2020-05-08 18:19:26 +0800

    utils function for load weights,nms,etc.

--- utils.py
+++ utils.py
@@ -89,7 +89,7 @@ def draw_bbox(image,bboxes,classes = read_class_names(cfg['yolo_classes_names'])
 
     for i,bbox in enumerate(bboxes):
         coordinates = np.array(bbox[:4],dtype = np.int32)
-        font_sacle = 0.5
+        font_scale = 0.5
         score = bbox[4]
         class_id = int(bbox[5])
         bbox_color = colors[class_id]
@@ -99,9 +99,9 @@ def draw_bbox(image,bboxes,classes = read_class_names(cfg['yolo_classes_names'])
 
         if show_label:
             bbox_mes = '%s: %.2f'%(classes[class_id],score)
-            t_size = cv2.getTextSize(bbox_mes,0,font_sacle,thickness = bbox_thick // 2)[0]
-            cv2.rectangle(image,c1,(c1[0] + t_size[0],c1[1] - t_size[1] -3),bbox_color,-1)
-            cv2.putText(image,bbox_mes,(c1[0],c1[1]-2),cv2.FONT_HERSHEY_SIMPLEX,font_scale,(0,0,0),
+            t_size = cv2.getTextSize(bbox_mes,0,font_scale,thickness = bbox_thick // 2)[0]
+            cv2.rectangle(image,coor1,(coor1[0] + t_size[0],coor1[1] - t_size[1] -3),bbox_color,-1)
+            cv2.putText(image,bbox_mes,(coor1[0],coor1[1]-2),cv2.FONT_HERSHEY_SIMPLEX,font_scale,(0,0,0),
                         bbox_thick // 2,lineType = cv2.LINE_AA)
 
         return image
@@ -152,7 +152,7 @@ def nms(bboxes,iou_threshold,sigma = 0.3,method = 'nms'):
             max_idx = np.argmax(cls_bboxes[:,4])
             best_bbox = cls_bboxes[max_idx]
             best_bboxes.append(best_bbox)
-            cls_bboxes = np.concatenate(cls_bboxes[:max_idx],cls_bboxes[max_idx+1:])
+            cls_bboxes = np.concatenate([cls_bboxes[:max_idx],cls_bboxes[max_idx+1:]])
             iou = bboxes_iou(best_bbox[np.newaxis,:4],cls_bboxes[:,:4])
             weight = np.ones((len(iou),),dtype = np.float32)
 
@@ -221,41 +221,43 @@ def postprocess_boxes(pred_bbox,org_img_shape,input_size,score_threshold):
     Return:
         
     """
-    valid_scale = [0,np.inf]
+    valid_scale=[0, np.inf]
     pred_bbox = np.array(pred_bbox)
 
-    pred_xywh = pred_bbox[:,0:4]
-    pred_conf = pred_bbox[:,4]
-    pred_prob = pred_bbox[:,5:]
+    pred_xywh = pred_bbox[:, 0:4]
+    pred_conf = pred_bbox[:, 4]
+    pred_prob = pred_bbox[:, 5:]
 
-    pred_coor = np.concatenate([pred_xywh[:,:2] - pred_xywh[:,2:4] * 0.5,
-                                pred_xywh[:,:2] + pred_xywh[:,2:4] * 0.5],axis = -1)
-
-    org_h,org_w = org_img_shape
-    resize_ratio = min(input_size / org_h,input_size / org_w)
+    pred_coor = np.concatenate([pred_xywh[:, :2] - pred_xywh[:, 2:] * 0.5,
+                                pred_xywh[:, :2] + pred_xywh[:, 2:] * 0.5], axis=-1)
+    org_h, org_w = org_img_shape
+    resize_ratio = min(input_size / org_w, input_size / org_h)
 
     dw = (input_size - resize_ratio * org_w) / 2
     dh = (input_size - resize_ratio * org_h) / 2
 
-    pred_coor[:,0::2] = 1.0 * (pred_coor[:,0::2] - dw) / resize_ratio
-    pred_coor[:,1::2] = 1.0 * (pred_coor[:,1::2] - dh) / resize_ratio
-
+    pred_coor[:, 0::2] = 1.0 * (pred_coor[:, 0::2] - dw) / resize_ratio
+    pred_coor[:, 1::2] = 1.0 * (pred_coor[:, 1::2] - dh) / resize_ratio
+    
     pred_coor = np.concatenate([np.maximum(pred_coor[:, :2], [0, 0]),
                                 np.minimum(pred_coor[:, 2:], [org_w - 1, org_h - 1])], axis=-1)
     invalid_mask = np.logical_or((pred_coor[:, 0] > pred_coor[:, 2]), (pred_coor[:, 1] > pred_coor[:, 3]))
     pred_coor[invalid_mask] = 0
-
+    
     bboxes_scale = np.sqrt(np.multiply.reduce(pred_coor[:, 2:4] - pred_coor[:, 0:2], axis=-1))
     scale_mask = np.logical_and((valid_scale[0] < bboxes_scale), (bboxes_scale < valid_scale[1]))
-
+    print("scale_mask:",scale_mask)
+    
     classes = np.argmax(pred_prob, axis=-1)
     scores = pred_conf * pred_prob[np.arange(len(pred_coor)), classes]
+    print("score:",scores)
     score_mask = scores > score_threshold
+    print("score_mask:",score_mask)
     mask = np.logical_and(scale_mask, score_mask)
+    print("mask",mask)
     coors, scores, classes = pred_coor[mask], scores[mask], classes[mask]
-
+    
     return np.concatenate([coors, scores[:, np.newaxis], classes[:, np.newaxis]], axis=-1)
-
 
 def load_partial_weights(model,weights_file):
 
